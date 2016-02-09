1cda95bec7279384d177f07659ad6ccdc7f6e9de
eahirst <elenahirst42@gmail.com>
2016-02-08 21:17:36 -0700

    Update avatarscript.js

--- avatarscript.js
+++ avatarscript.js
@@ -3,12 +3,22 @@ angular.module('app', [])
 
 function mainCtrl ($scope) {
   
+  // tada! a users array will now be bound to and made available to our html template
+  $scope.users = [];
+  
   /**
-   * Let's just make sure something happens when the user submits the form
-   * by binding the declared 'addNew' function to the scope. You can see we
-   * are expecting a user object as a parameter. This is 'userForm'
+   * 1. We push an new `user` object to our users list with a name
+   * and avatarUrl property
+   * 2. For our purposes, a quick and dirty method for clearing the form will due.
+   * Just note there is a more appropriate method when form validation is involved.
    */
   $scope.addNew = function (user) {
-    alert(user.name + ' ' + user.url);
+    $scope.users.push({ 
+      name: user.name,
+      avatarUrl: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+    user.url = ''; /* [2] */
   };
 }
