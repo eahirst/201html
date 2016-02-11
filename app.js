d5ccad3367a290508d7e58596512b9533ab6d813
eahirst <elenahirst42@gmail.com>
2016-02-11 09:40:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ angular.module('News', [])
   '$scope',
   function($scope){
     $scope.test = 'Hello world!';
+    $scope.posts = [
+      'Post 1',
+      'Post 2',
+      'Post 3',
+      'Post 4',
+      'Post 5'
+    ];
   }
 ]);
